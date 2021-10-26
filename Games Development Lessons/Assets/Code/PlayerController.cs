14d6ac541f6baba6d47bb0bcfca973510d9e4e1b
Elliot <elliotgriffiss@gmail.com>
2021-10-26 15:56:12 +0100

    Controller Improvements
    - Fixed animation bug
    - Adjusted the size of spike colliders
    - Added code to debug ground detection
    - Swapped Raycast to BoxCast

--- Games Development Lessons/Assets/Code/PlayerController.cs
+++ Games Development Lessons/Assets/Code/PlayerController.cs
@@ -19,54 +19,71 @@ public class PlayerController : MonoBehaviour
     private Vector3 RightFacing = new Vector3(1, 1, 1);
     private Vector3 LeftFacing = new Vector3(-1, 1, 1);
 
+    [SerializeField] private Vector2 Scale;
+
+    // Controller Inputs
+
+    private float direction;
+    private bool jump;
+
+
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, Vector2.down, 1.5f, Mask);
+        direction = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetButtonDown("Jump");
+    }
+
+    /// <summary>
+    /// Frame-rate independent MonoBehaviour.FixedUpdate message for physics calculations.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(gameObject.transform.position, Scale, 0f, Vector2.down, 1.5f, Mask);
         isGrounded = (hit.collider != null);
-        float direction = Input.GetAxis("Horizontal");
+
+        DebugCollision(gameObject.transform.position, gameObject.transform.localScale, isGrounded);
 
         if (direction != 0)
         {
             Player.transform.position += Vector3.right * direction * Time.deltaTime * Speed;
             Animator.SetBool("IsWalking", true);
 
-            if (direction>0)
+            if (direction > 0)
             {
                 transform.localScale = RightFacing;
             }
-            else if(direction<0)
+            else if (direction < 0)
             {
                 transform.localScale = LeftFacing;
             }
-        }   
+        }
         else
         {
             Animator.SetBool("IsWalking", false);
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (jump && isGrounded)
         {
             Player.AddForce(JumpForce, ForceMode2D.Impulse);
             Animator.SetBool("IsJumping", true);
         }
 
-
-
-        Animator.SetBool("IsJumping", !isGrounded);
-
-        if (gameObject.transform.position.y<-5)
+        if (gameObject.transform.position.y < -5)
         {
             RespawnPlayer();
         }
+
+        Debug.Log(direction);
+        Animator.SetBool("IsJumping", !isGrounded);
     }
 
     // called when the cube hits the floor
-    void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("OnCollisionEnter2D");
         
-        if(col.gameObject.layer == 9)
+        if(collision.gameObject.layer == 9)
         {
             Animator.SetBool("IsDieing", true);
            
@@ -77,5 +94,19 @@ public class PlayerController : MonoBehaviour
     { 
         gameObject.transform.position = spawmpoint.transform.position;
         Animator.SetBool("IsDieing", false);
+    }
+
+    private void DebugCollision(Vector2 boxCenter, Vector2 boxSize, bool successful)
+    {
+        Vector2 t_tL = boxCenter + new Vector2(-boxSize.x * 0.5f, boxSize.y * 0.5f);
+        Vector2 t_tR = boxCenter + new Vector2(boxSize.x * 0.5f, boxSize.y * 0.5f);
+        Vector2 t_bL = boxCenter + new Vector2(-boxSize.x * 0.5f, -boxSize.y * 0.5f);
+        Vector2 t_bR = boxCenter + new Vector2(boxSize.x * 0.5f, -boxSize.y * 0.5f);
+        Color t_lineColor = (successful) ? Color.green : Color.red;
+
+        Debug.DrawLine(t_tL, t_tR, t_lineColor);
+        Debug.DrawLine(t_bL, t_bR, t_lineColor);
+        Debug.DrawLine(t_tL, t_bL, t_lineColor);
+        Debug.DrawLine(t_tR, t_bR, t_lineColor);
     }
 }
